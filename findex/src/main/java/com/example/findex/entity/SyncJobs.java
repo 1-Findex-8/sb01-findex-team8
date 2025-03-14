811d6a018503e6b29a1f54d6c897eb29698de3fd
김주언 <wndjs803@naver.com>
2025-03-14 11:23:42 +0900

    feat: SyncJobs Entity 정의

---
+++ findex/src/main/java/com/example/findex/entity/SyncJobs.java
@@ -0,0 +1,41 @@
+package com.example.findex.entity;
+
+import com.example.findex.entity.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SyncJobs extends BaseEntity {
+
+  @Column(nullable = false)
+  @Enumerated(value = EnumType.STRING)
+  private JobType jobType;
+
+  private LocalDate targetDate;
+
+  @Column(nullable = false)
+  private String worker;
+
+  @Column(nullable = false)
+  private LocalDateTime jobTime;
+
+  @Column(nullable = false)
+  @Enumerated(value = EnumType.STRING)
+  private Result result;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "index_info_id", nullable = false)
+  private IndexInfo indexInfo;
+}
