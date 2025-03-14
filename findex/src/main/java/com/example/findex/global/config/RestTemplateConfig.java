049ae05e7ab26e60be54b6e87b3996e26dc31810
김주언 <wndjs803@naver.com>
2025-03-14 16:38:24 +0900

    feat: RestTemplate 요청 제한 시간 설정

--- findex/src/main/java/com/example/findex/global/config/RestTemplateConfig.java
+++ findex/src/main/java/com/example/findex/global/config/RestTemplateConfig.java
@@ -1,17 +1,19 @@
 package com.example.findex.global.config;
 
+import java.time.Duration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestClientConfig {
+public class RestTemplateConfig {
 
   @Bean
   public RestTemplate restTemplate() {
-    return new RestTemplate();
+    return new RestTemplateBuilder()
+        .connectTimeout(Duration.ofSeconds(3))
+        .readTimeout(Duration.ofSeconds(3))
+        .build();
   }
 }
