cedda4af92d2b424156f82d76ce45ac6bda2a6fe
yubin8773 <hiyubin.lee@gmail.com>
2025-03-17 15:18:20 +0900

    fix: Delete toEntity method

--- findex/src/main/java/com/example/findex/mapper/IndexInfoMapper.java
+++ findex/src/main/java/com/example/findex/mapper/IndexInfoMapper.java
@@ -2,9 +2,6 @@ package com.example.findex.mapper;
 
 import com.example.findex.dto.IndexInfo.IndexInfoDto;
 import com.example.findex.entity.IndexInfo;
-import com.example.findex.entity.SourceType;
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,17 +9,15 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class IndexInfoMapper {
 
-  public IndexInfo toEntity(String indexClassification, String indexName,
-      int employeeItemsCount, LocalDate basePointInTime, BigDecimal baseIndex, SourceType sourceType,
-      boolean favorite) {
-    return new IndexInfo(indexClassification, indexName, employeeItemsCount, basePointInTime, baseIndex, sourceType,
-        favorite);
-  }
-
   public IndexInfoDto toDto(IndexInfo indexInfo) {
-    return new IndexInfoDto(indexInfo.getId(), indexInfo.getIndexClassification(),
-        indexInfo.getIndexName(), indexInfo.getEmployeeItemsCount(),
-        indexInfo.getBasePointInTime(), indexInfo.getBaseIndex(),
-        indexInfo.getSourceType(), indexInfo.isFavorite());
+    return new IndexInfoDto(
+        indexInfo.getId(),
+        indexInfo.getIndexClassification(),
+        indexInfo.getIndexName(),
+        indexInfo.getEmployeeItemsCount(),
+        indexInfo.getBasePointInTime(),
+        indexInfo.getBaseIndex(),
+        indexInfo.getSourceType(),
+        indexInfo.isFavorite());
   }
 }
