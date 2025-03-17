c918af7c3ca1a7c6e3f8f5879cbca176bec7cdd0
Son DongHyuk <sond5510@naver.com>
2025-03-17 17:48:00 +0900

    refactor : IndexDataMapper(use Mapstruct)

--- findex/src/main/java/com/example/findex/mapper/IndexDataMapper.java
+++ findex/src/main/java/com/example/findex/mapper/IndexDataMapper.java
@@ -2,37 +2,11 @@ package com.example.findex.mapper;
 
 import com.example.findex.dto.indexdata.data.IndexDataDto;
 import com.example.findex.entity.IndexData;
-import com.example.findex.entity.IndexInfo;
-import com.example.findex.entity.SourceType;
-import com.example.findex.repository.IndexInfoRepository;
-import java.util.NoSuchElementException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-@Component
-@RequiredArgsConstructor
+@Mapper(componentModel = "spring")
+public interface IndexDataMapper {
 
-public class IndexDataMapper {
-  private final IndexInfoRepository indexInfoRepository;
-
-  public IndexDataDto toDto(IndexData indexData) {
-    IndexInfo indexInfo = indexInfoRepository.findById(indexData.getId()).orElseThrow(()->new NoSuchElementException("IndexInfo with id "+indexData.getId()+ "not found"));
-
-    return new IndexDataDto(
-        indexData.getId(),
-        indexInfo.getId(),
-        indexData.getBaseDate(),
-        SourceType.USER,
-        indexData.getMarketPrice(),
-        indexData.getClosingPrice(),
-        indexData.getHighPrice(),
-        indexData.getLowPrice(),
-        indexData.getVersus(),
-        indexData.getFluctuationRate(),
-        indexData.getTradingQuantity(),
-        indexData.getTradingPrice(),
-        indexData.getMarketTotalAmount()
-    );
-  }
-
+  IndexDataDto toDto(IndexData indexData);
 }
