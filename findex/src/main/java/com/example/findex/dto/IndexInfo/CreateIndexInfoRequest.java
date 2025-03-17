b40358332499b831934e28596b5855ae8b1d6b6b
yubin8773 <hiyubin.lee@gmail.com>
2025-03-17 15:28:41 +0900

    style: Add first line enter

--- findex/src/main/java/com/example/findex/dto/IndexInfo/CreateIndexInfoRequest.java
+++ findex/src/main/java/com/example/findex/dto/IndexInfo/CreateIndexInfoRequest.java
@@ -3,11 +3,11 @@ package com.example.findex.dto.IndexInfo;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record CreateIndexInfoRequest(String indexClassification,
-                                     String indexName,
-                                     int employedItemsCount,
-                                     LocalDate basePointInTime,
-                                     BigDecimal baseIndex,
-                                     boolean favorite) {
-
+public record CreateIndexInfoRequest(
+    String indexClassification,
+    String indexName,
+    int employedItemsCount,
+    LocalDate basePointInTime,
+    BigDecimal baseIndex,
+    boolean favorite) {
 }
--- findex/src/main/java/com/example/findex/dto/IndexInfo/IndexInfoDto.java
+++ findex/src/main/java/com/example/findex/dto/IndexInfo/IndexInfoDto.java
@@ -4,12 +4,13 @@ import com.example.findex.entity.SourceType;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record IndexInfoDto(Long id,
-                           String indexClassification,
-                           String indexName,
-                           int employeeItemsCount,
-                           LocalDate basePointInTime,
-                           BigDecimal baseIndex,
-                           SourceType sourceType,
-                           boolean favorite) {
+public record IndexInfoDto(
+    Long id,
+    String indexClassification,
+    String indexName,
+    int employeeItemsCount,
+    LocalDate basePointInTime,
+    BigDecimal baseIndex,
+    SourceType sourceType,
+    boolean favorite) {
 }
