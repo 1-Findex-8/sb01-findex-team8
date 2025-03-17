8c6d0ed49754c66b4a4425dd744f1e515ca6f3fd
김주언 <wndjs803@naver.com>
2025-03-17 16:09:26 +0900

    fix: BusinessException 생성자, ErrorCode 구조 변경

--- findex/src/main/java/com/example/findex/global/error/ErrorCode.java
+++ findex/src/main/java/com/example/findex/global/error/ErrorCode.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
   // 예시
-  USER_NOT_FOUND(400, "유저를 찾지 못했습니다", "잘못된 ID 입니다."),
+//  USER_NOT_FOUND(400, "유저를 찾지 못했습니다", "잘못된 ID 입니다."),
 
   // indexinfo
 
@@ -14,15 +14,14 @@ public enum ErrorCode {
   // syncjobs
 
   // autosyncconfigs
-  AUTOSYNCCONFIGS_NOT_FOUND(404, "auto sync config를 찾지 못했습니다", "존재하지 않는 id입니다.");
+  AUTOSYNCCONFIGS_NOT_FOUND(404, "auto sync config를 찾지 못했습니다");
 
   private final int status;
   private final String message;
-  private final String details;
 
-  ErrorCode(int status, String message, String details) {
+  ErrorCode(int status, String message) {
     this.status = status;
     this.message = message;
-    this.details = details;
   }
+
 }
