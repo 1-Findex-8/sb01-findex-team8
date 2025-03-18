eb33995fb4255a1841c32ac7da98f7c1101f1baf
Son DongHyuk <sond5510@naver.com>
2025-03-18 16:06:05 +0900

    feat : Exception 추가

--- findex/src/main/java/com/example/findex/controller/IndexDataController.java
+++ findex/src/main/java/com/example/findex/controller/IndexDataController.java
@@ -4,13 +4,16 @@ import com.example.findex.api.IndexDataApi;
 import com.example.findex.dto.indexdata.data.IndexDataDto;
 import com.example.findex.dto.indexdata.request.IndexDataCreateRequest;
 import com.example.findex.dto.indexdata.response.CursorPageResponseIndexDataDto;
-import com.example.findex.dto.indexdata.response.ErrorResponse;
 import com.example.findex.dto.indexdata.response.IndexPerformanceDto;
+import com.example.findex.global.error.ErrorCode;
+import com.example.findex.global.error.exception.BusinessException;
+import com.example.findex.global.error.exception.indexdata.IndexDataIntegrityViolationException;
+import com.example.findex.global.error.exception.indexdata.IndexDataInternalServerErrorException;
+import com.example.findex.global.error.exception.indexdata.IndexDataNoSuchElementException;
 import com.example.findex.service.IndexDataService;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -44,14 +47,11 @@ public class IndexDataController implements IndexDataApi {
       IndexDataDto indexDto = indexDataService.create(indexDataCreateRequest);
       return ResponseEntity.status(HttpStatus.CREATED).body(indexDto);
     }catch (DataIntegrityViolationException e) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-          .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "중복된 지수 및 날짜 조합입니다.", e.getMessage()));
+      throw new IndexDataIntegrityViolationException(ErrorCode.INDEX_INTERNAL_SERVER_ERROR.getMessage());
     } catch (NoSuchElementException e) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND)
-          .body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), "해당 ID의 지수 정보를 찾을 수 없습니다.", e.getMessage()));
+      throw new IndexDataNoSuchElementException(ErrorCode.INDEX_NOT_FOUND.getMessage());
     } catch (Exception e) {
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-          .body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 내부 오류가 발생했습니다.", e.getMessage()));
+      throw new IndexDataInternalServerErrorException(ErrorCode.INDEX_INTERNAL_SERVER_ERROR.getMessage());
     }
   }
 
--- findex/src/main/java/com/example/findex/global/error/exception/BusinessException.java
+++ findex/src/main/java/com/example/findex/global/error/exception/BusinessException.java
@@ -2,12 +2,21 @@ package com.example.findex.global.error.exception;
 
 import com.example.findex.global.error.ErrorCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 public class BusinessException extends RuntimeException {
   private final ErrorCode errorCode;
+  private String details = ""; // details는 필수적이지 않다고 생각해서 ""로 초기화
 
   public BusinessException(ErrorCode errorCode) {
     this.errorCode = errorCode;
   }
+
+  // details가 포함된 생성자로 변경 가능
+  public BusinessException(ErrorCode errorCode, String details) {
+    this.errorCode = errorCode;
+    this.details = details;
+  }
+
 }
