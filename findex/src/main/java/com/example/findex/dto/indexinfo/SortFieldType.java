22303b282025b31dd286960b30bfed1bbd111c44
yubin8773 <hiyubin.lee@gmail.com>
2025-03-19 13:09:56 +0900

    feat: Add Custom Error

--- findex/src/main/java/com/example/findex/dto/indexinfo/SortFieldType.java
+++
@@ -1,5 +0,0 @@
-package com.example.findex.dto.indexinfo;
-
-public enum SortFieldType {
-  indexClassification, indexName, employedItemsCount
-}
--- findex/src/main/java/com/example/findex/global/error/exception/indexinfo/IndexInfoNotFoundException.java
+++ findex/src/main/java/com/example/findex/global/error/exception/indexinfo/IndexInfoNotFoundException.java
@@ -6,6 +6,9 @@ import com.example.findex.global.error.exception.BusinessException;
 public class IndexInfoNotFoundException extends BusinessException {
 
   public IndexInfoNotFoundException() {
-    super(ErrorCode.INDEXINFO_NOT_FOUND);
+    super(ErrorCode.INDEX_INFO_NOT_FOUND);
+  }
+  public IndexInfoNotFoundException(String message) {
+    super(ErrorCode.INDEX_INFO_NOT_FOUND, message);
   }
 }
