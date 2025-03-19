6cc313f9aef61e3b4c575475dd85f0f0d58484f0
yubin8773 <hiyubin.lee@gmail.com>
2025-03-19 13:46:33 +0900

    feat: Add mapper(toSummaryDto) to convert IndexInfo to IndexInfoSummaryDto (#29)

--- findex/src/main/java/com/example/findex/mapper/IndexInfoMapper.java
+++ findex/src/main/java/com/example/findex/mapper/IndexInfoMapper.java
@@ -1,11 +1,21 @@
 package com.example.findex.mapper;
 
 import com.example.findex.dto.indexinfo.IndexInfoDto;
+import com.example.findex.dto.indexinfo.IndexInfoSummaryDto;
 import com.example.findex.entity.IndexInfo;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IndexInfoMapper {
 
   IndexInfoDto toDto(IndexInfo indexInfo);
+
+  @Mapping(target = "employeeItemsCount", ignore = true)
+  @Mapping(target = "employeeItemsCount", ignore = true)
+  @Mapping(target = "basePointInTime", ignore = true)
+  @Mapping(target = "baseIndex", ignore = true)
+  @Mapping(target = "sourceType", ignore = true)
+  @Mapping(target = "favorite", ignore = true)
+  IndexInfoSummaryDto toSummaryDto(IndexInfo indexInfo);
 }
