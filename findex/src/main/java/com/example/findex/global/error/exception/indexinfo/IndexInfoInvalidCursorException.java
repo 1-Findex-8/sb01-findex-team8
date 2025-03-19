11f70c7022cd9c1a93b2c1b01a7b51625f8848e8
iiyubb <hiyubin.lee@gmail.com>
2025-03-20 00:43:32 +0900

    fix: Modify index info custom exception

--- findex/src/main/java/com/example/findex/global/error/exception/indexinfo/IndexInfoInvalidCursorException.java
+++ findex/src/main/java/com/example/findex/global/error/exception/indexinfo/IndexInfoInvalidCursorException.java
@@ -3,9 +3,9 @@ package com.example.findex.global.error.exception.indexinfo;
 import com.example.findex.global.error.ErrorCode;
 import com.example.findex.global.error.exception.BusinessException;
 
-public class IndexInfoInvalidCursor extends BusinessException {
-  public IndexInfoInvalidCursor() { super(ErrorCode.INDEX_INFO_INVALID_CURSOR); }
-  public IndexInfoInvalidCursor(String message) {
+public class IndexInfoInvalidCursorException extends BusinessException {
+  public IndexInfoInvalidCursorException() { super(ErrorCode.INDEX_INFO_INVALID_CURSOR); }
+  public IndexInfoInvalidCursorException(String message) {
     super(ErrorCode.INDEX_INFO_INVALID_CURSOR, message);
   }
 
