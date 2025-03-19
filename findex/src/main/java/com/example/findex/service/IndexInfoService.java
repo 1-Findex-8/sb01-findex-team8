4bdd14b0bcec7bd6e2b25222132658c68716b7cc
yubin8773 <hiyubin.lee@gmail.com>
2025-03-19 09:24:43 +0900

    feat: Add condition filtering query

--- findex/src/main/java/com/example/findex/service/IndexInfoService.java
+++ findex/src/main/java/com/example/findex/service/IndexInfoService.java
@@ -16,8 +16,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
